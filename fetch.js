dc592172a2a6b107778aa6af747f828db59a761d
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-07-16 21:20:16 -0400

    Fetch Sorted, Startin' CSS

--- fetch.js
+++ fetch.js
@@ -1,12 +1,6 @@
-// let pokemon = [
-//     1
-// ]
-
-// fetch('https://pokeapi.co/api/v2/type/'+pokemon[0]+'/')
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-let pokeName = []
+let curPoke = []
 let promptTest = prompt('Give us a name');
+// const nameThat = document.getElementById('name');
 
 let poke = new Promise((resolve, reject) => {
     resolve(
@@ -15,45 +9,42 @@ let poke = new Promise((resolve, reject) => {
     )    
 })
 
-setTimeout(() => {
-    poke.then(data => pokeName.name = data.name)
-    poke.then(data => pokeName.id = data.id)
-    poke.then(data => pokeName.height = data.height)
-    poke.then(data => pokeName.weight = data.weight)
-    poke.then(data => pokeName.typeOne = data.types[0].type.name)
-    poke.then(data => pokeName.typeTwo = data.types[1].type.name)
-}, 1000);
+// API Data to pokeName Array
+poke.then(data => curPoke.name = data.name)                          
+poke.then(data => curPoke.id = data.id)
+poke.then(data => curPoke.height = data.height)
+poke.then(data => curPoke.weight = data.weight) 
+poke.then(data => curPoke.typeOne = data.types[0].type.name)
+poke.then(data => curPoke.typeTwo = data.types[1].type.name)
+poke.then(data => curPoke.sprite = data.sprites.front_default)
 
+// Effectiveness Data to Array
 setTimeout(() => {
-    let poke2 = new Promise((resolve, reject) => {
+    let pokeDmg = new Promise((resolve, reject) => {
         resolve(
-            fetch('https://pokeapi.co/api/v2/type/' + pokeName.id + '/')
+            fetch('https://pokeapi.co/api/v2/type/' + curPoke.id + '/')
                 .then(res => res.json())
         )       
     })
-    poke2.then(data => pokeName.effective = data.damage_relations)
+    let pokeImg = new Promise((resolve, reject) => {
+        resolve(
+            fetch('https://pokeres.bastionbot.org/images/pokemon/' + curPoke.id + '.png')
+        )
+    })
 
-}, 2000);
+    pokeDmg.then(data => curPoke.dmg = data.damage_relations)           // Damage Effectiveness
+    pokeImg.then(data => curPoke.img = data.url)                                    // Large Img
 
-// let nameTest = poke.then(theName => pokeName.name = theName.name)
-// let heightTest = poke.then(theHeight => pokeName.height = theHeight.height)    
-// poke.then(data => pokeName.name = data.name)
-// poke.then(data => pokeName.id = data.id)
-// poke.then(data => pokeName.height = data.height)
-// poke.then(data => pokeName.weight = data.weight)
-// poke.then(data => pokeName.typeOne = data.types[0].type.name)
-// poke.then(data => pokeName.typeTwo = data.types[1].type.name)
-// poke2.then(data => pokeName.effective = data.damage_relations)
+}, 1000);
 
 const testing = document.getElementById('testing');
 let li = document.createElement('li');
 
 
 setTimeout(() => {
-        // li.innerHTML = pokeName.name;
+        // li.innerHTML = curPoke.name;
         // testing.append(li);
-        console.log(pokeName)
-        // console.log(pokeName.effective.double_damage_from[0].name);
-
-    }, 5000);
-
+        console.log(curPoke);
+        // console.log(curPoke.img)
+        // console.log(curPoke.dmg)
+    }, 2000);
