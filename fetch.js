d50362fa2a3fbd5939881062acd6d90dc98a4412
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-07-20 01:16:14 -0400

    CSS Layout / Mobile Layout, Fixed heightAdjust Func

--- fetch.js
+++ fetch.js
@@ -1,7 +1,8 @@
 let curPoke = []
 let curPokeStats = []
 let promptTest = prompt('Give us a name');
-// const nameThat = document.getElementById('name');
+// let promptTest = 1;
+// const searchPoke = document.querySelector('.search').value;
 
 let poke = new Promise((resolve, reject) => {
     resolve(
@@ -28,7 +29,7 @@ poke.then(data => curPokeStats.spd = data.stats[5].base_stat);
 // Checks for typeTwo (avoids bug if there is only one type)
 function multiType() {
     if (!curPoke.typeTwo) {
-        console.log('Only have one Type');
+        console.log('This Pokemon only has one type. (Line 21 Error)');
     } else {
         typeBar.insertAdjacentHTML('beforeend', `<h3 class="type ${curPoke.typeTwo}">${upperCase(curPoke.typeTwo)}</h3>`);
     }
@@ -59,11 +60,11 @@ function upperCase(string) {
 // Turn Integer into Feet / Inches
 function heightAdjust(num) {        //--- Needs Adjusted for Tall Pokemon
     num = num * 0.10;
-    num = num * 3.281;
+    num = num * 39.37;
+    num = num / 12;
     foot = num.toString();
-    foot = foot.substring(0,1)
-    inch = num.toString();
-    inch = inch.slice(1);
+    foot = Math.floor(num);
+    inch = num - foot;
     inch = 12 * inch;
     inch = Math.ceil(inch);
     return `${foot}'${inch}" `
@@ -83,14 +84,14 @@ function percentage(num) {
     return num
 }
 
-
 setTimeout(() => {
+        document.querySelector(".container").style.display= "grid";
         imgDiv.innerHTML = '<img src="' + curPoke.img + '" class="image">';
         pokeInfo.insertAdjacentHTML('afterbegin', `<h1 class="pokeName">${curPoke.name}</h1>`)
         pokeInfo.insertAdjacentHTML('beforeend', `<h3 class="pokeID"># ${curPoke.id}</h3>`)
         pokeHW.insertAdjacentHTML('afterbegin', `<h3 class="height">Height: ${heightAdjust(curPoke.height)}</h3>`)
         pokeHW.insertAdjacentHTML('beforeend', `<h3 class="weight">Weight: ${weightAdjust(curPoke.weight)}</h3>`)
-        pokeHW.insertAdjacentHTML('beforeend', `<h3 class="gender">Abilities: ${upperCase(curPoke.abilities[0].ability.name)},  ${upperCase(curPoke.abilities[1].ability.name)}</h3>`)
+        pokeHW.insertAdjacentHTML('beforeend', `<h3 class="ability">Abilities: ${upperCase(curPoke.abilities[0].ability.name)},  ${upperCase(curPoke.abilities[1].ability.name)}</h3>`)
         typeBar.insertAdjacentHTML('afterbegin', `<h3 class=" type ${curPoke.typeOne}">${upperCase(curPoke.typeOne)}</h3>`)
         // typeBar.insertAdjacentHTML('beforeend', `<h3 class="type ${curPoke.typeTwo}">${upperCase(curPoke.typeTwo)}</h3>`)
         multiType();
@@ -100,37 +101,6 @@ setTimeout(() => {
         document.querySelector(".spd").style.width=`${percentage(curPokeStats.spd)}%`;
         document.querySelector(".satk").style.width=`${percentage(curPokeStats.satk)}%`;
         document.querySelector(".sdef").style.width=`${percentage(curPokeStats.sdef)}%`;
-    }, 2000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-           // function doubleDmg() {
-        //     let doubleDmgFrom = curPoke.dmg.double_damage_from;
-        //     let doubleDmgFromArray = []
-        //     for (let i = 0; i <= doubleDmgFrom.length; i++ ) {
-        //         doubleDmgFromArray = curPoke.dmg.double_damage_from[i].name;
-        //         pokeData.insertAdjacentHTML('afterbegin', `<p class="dmgtype ${doubleDmgFromArray}">2X - ${upperCase(doubleDmgFromArray)}</p></div>`);
-        //     };
-        // };
-
-        // function halfDmg() {
-        //     let halfDmgFrom = curPoke.dmg.double_damage_from;
-        //     let halfDmgFromArray = []
-        //     for (let i = 0; i <= doubleDmgFrom.length; i++ ) {
-        //         halfDmgFromArray = curPoke.dmg.half_damage_from[i].name;
-        //         console.log(halfDmgFromArray);
-        //         pokeData.insertAdjacentHTML('afterbegin', `<p class="dmgtype ${halfDmgFromArray}">2X - ${upperCase(halfDmgFromArray)}</p></div>`);
-        //     };
-        // };
+        console.log(curPoke.height);
+    }, 1500);
