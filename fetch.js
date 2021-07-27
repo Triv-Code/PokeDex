5608b96a2ac6e717fab6d59213f0591962be86bb
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-07-27 19:17:43 -0400

    Refactored Code, Fixed typeTwo Bug

--- fetch.js
+++ fetch.js
@@ -2,49 +2,6 @@ let curPoke = []
 let curPokeStats = []
 let promptTest = prompt('Give us a name').toLowerCase();
 
-let poke = new Promise((resolve, reject) => {
-    resolve(
-        fetch('https://pokeapi.co/api/v2/pokemon/' + promptTest + '/')
-            .then(res => res.json())
-    ) 
-})
-// API Data to pokeName Array
-poke.then(data => curPoke.name = data.name)                          
-poke.then(data => curPoke.id = data.id)
-poke.then(data => curPoke.height = data.height)
-poke.then(data => curPoke.weight = data.weight)
-poke.then(data => curPoke.abilities = data.abilities)
-poke.then(data => curPoke.abilities = data.abilities)
-poke.then(data => curPoke.typeOne = data.types[0].type.name)
-poke.then(data => curPoke.typeTwo = data.types[1].type.name)
-poke.then(data => curPoke.sprite = data.sprites.front_default)
-poke.then(data => curPokeStats.hp = data.stats[0].base_stat);
-poke.then(data => curPokeStats.atk = data.stats[1].base_stat);
-poke.then(data => curPokeStats.def = data.stats[2].base_stat);
-poke.then(data => curPokeStats.satk = data.stats[3].base_stat);
-poke.then(data => curPokeStats.sdef = data.stats[4].base_stat);
-poke.then(data => curPokeStats.spd = data.stats[5].base_stat);
-// Checks for typeTwo (Avoids a Bug if Pokemon only has 1 type, error is expected)
-function multiType() {
-    if (!curPoke.typeTwo) {
-        console.log('This Pokemon only has one type. (Line 19 Error)');
-    } else {
-        typeBar.insertAdjacentHTML('beforeend', `<h3 class="type ${curPoke.typeTwo}">${upperCase(curPoke.typeTwo)}</h3>`);
-    }
-
-}
-
-// Large Image Fetch... Delayed for curPoke.id to update. 
-setTimeout(() => {
-    let pokeImg = new Promise((resolve, reject) => {
-        resolve(
-            fetch('https://pokeres.bastionbot.org/images/pokemon/' + curPoke.id + '.png')
-        )
-    })
-    pokeImg.then(data => curPoke.img = data.url)
-
-}, 1000);
-
 const imgDiv = document.querySelector('.imgDiv');
 const pokeInfo = document.querySelector('.pokeInfo');
 const pokeHW = document.querySelector('.pokeHW');
@@ -66,7 +23,7 @@ function heightAdjust(num) {        //--- Needs Adjusted for Tall Pokemon
     inch = 12 * inch;
     inch = Math.ceil(inch);
     return `${foot}'${inch}" `
-  };
+};
 // Turns Integer into Pounds / Ounces
 function weightAdjust(num) {
     num = num * 0.1;
@@ -81,9 +38,29 @@ function percentage(num) {
     num = Math.floor(num);
     return num
 }
-
-setTimeout(() => {
-        document.querySelector(".container").style.display= "grid";
+// API Fetch, Creates Arrays, Inserts HTML
+fetch('https://pokeapi.co/api/v2/pokemon/' + promptTest + '/')
+    .then(res => res.json())
+    .then(data => {
+        curPoke.name = data.name
+        curPoke.id = data.id
+        curPoke.height = data.height
+        curPoke.weight = data.weight
+        curPoke.abilities = data.abilities
+        curPoke.abilities = data.abilities
+        curPoke.typeOne = data.types[0].type.name
+        if (data.types.length > 1) {
+            curPoke.typeTwo = data.types[1].type.name
+        }
+        curPoke.sprite = data.sprites.front_default
+        curPokeStats.hp = data.stats[0].base_stat
+        curPokeStats.atk = data.stats[1].base_stat
+        curPokeStats.def = data.stats[2].base_stat
+        curPokeStats.satk = data.stats[3].base_stat
+        curPokeStats.sdef = data.stats[4].base_stat
+        curPokeStats.spd = data.stats[5].base_stat
+        curPoke.img = 'https://pokeres.bastionbot.org/images/pokemon/' + data.id + '.png'
+        document.querySelector(".container").style.display = "grid";
         imgDiv.innerHTML = '<img src="' + curPoke.img + '" class="image">';
         pokeInfo.insertAdjacentHTML('afterbegin', `<h1 class="pokeName">${curPoke.name}</h1>`)
         pokeInfo.insertAdjacentHTML('beforeend', `<h3 class="pokeID"># ${curPoke.id}</h3>`)
@@ -91,12 +68,14 @@ setTimeout(() => {
         pokeHW.insertAdjacentHTML('beforeend', `<h3 class="weight">Weight: ${weightAdjust(curPoke.weight)}</h3>`)
         pokeHW.insertAdjacentHTML('beforeend', `<h3 class="ability">Abilities: ${upperCase(curPoke.abilities[0].ability.name)},  ${upperCase(curPoke.abilities[1].ability.name)}</h3>`)
         typeBar.insertAdjacentHTML('afterbegin', `<h3 class=" type ${curPoke.typeOne}">${upperCase(curPoke.typeOne)}</h3>`)
-        // typeBar.insertAdjacentHTML('beforeend', `<h3 class="type ${curPoke.typeTwo}">${upperCase(curPoke.typeTwo)}</h3>`)
-        multiType();
-        document.querySelector(".hp").style.width=`${percentage(curPokeStats.hp)}%`;
-        document.querySelector(".atk").style.width=`${percentage(curPokeStats.atk)}%`;
-        document.querySelector(".def").style.width=`${percentage(curPokeStats.def)}%`;
-        document.querySelector(".spd").style.width=`${percentage(curPokeStats.spd)}%`;
-        document.querySelector(".satk").style.width=`${percentage(curPokeStats.satk)}%`;
-        document.querySelector(".sdef").style.width=`${percentage(curPokeStats.sdef)}%`;
-    }, 2000);
+        if (curPoke.typeTwo) {
+            typeBar.insertAdjacentHTML('beforeend', `<h3 class="type ${curPoke.typeTwo}">${upperCase(curPoke.typeTwo)}</h3>`);
+        }
+        document.querySelector(".hp").style.width = `${percentage(curPokeStats.hp)}%`;
+        document.querySelector(".atk").style.width = `${percentage(curPokeStats.atk)}%`;
+        document.querySelector(".def").style.width = `${percentage(curPokeStats.def)}%`;
+        document.querySelector(".spd").style.width = `${percentage(curPokeStats.spd)}%`;
+        document.querySelector(".satk").style.width = `${percentage(curPokeStats.satk)}%`;
+        document.querySelector(".sdef").style.width = `${percentage(curPokeStats.sdef)}%`;
+    })
+    .catch(() => alert('not a pokemon'))
